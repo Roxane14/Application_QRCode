bae98e6b9deda087eb555eb15243325df381f9f1
Roxane <roxane.le_tensorer.etu@univ-lemans.fr>
2020-05-13 14:05:33 +0200

    Accès BDD
    On peut maintenant créer des histoires et des étapes qui en font partie. Quelques bugs persistent : il faut cliquer deux fois pour créer l'histoire, et la problème du burger-menu (qui arrête de fonctionner au bout d'un moment) n'est pas résolu.

--- src/app/FirebaseService.ts
+++ src/app/FirebaseService.ts
@@ -3,9 +3,12 @@ import { AngularFirestore } from 'angularfire2/firestore'
 import {
   Injectable
 } from '@angular/core';
+import { stringify } from 'querystring';
 
 @Injectable()
 export class FirebaseService {
+
+  public reference:string;
 
     constructor(
       public afs: AngularFirestore
@@ -26,4 +29,48 @@ export class FirebaseService {
           )
         })
       }
+
+      nouvelleHistoire(titre:string, nombre:number, etapes:Etape[]){
+        
+        return new Promise<any>((resolve, reject) => {
+          this.afs.collection('/histoires').add({
+            Titre: titre,
+            NombreEtapes: nombre
+          })
+          .then(
+            (res) => {
+              resolve(res)
+              this.reference=res.id;
+            },
+            err => reject(err)
+          );
+
+          for(let i=0;i<nombre;i++){
+
+            this.afs.collection('/histoires/').doc(this.reference).collection('/etapes/').add({
+              Nom: etapes[i].nom,
+              Lieu: etapes[i].lieu
+            })
+            .then(
+              (res) => {
+                resolve(res)
+              },
+              err => reject(err)
+            )
+
+          }
+          
+        })
+      }
+  }
+
+  @Injectable()
+  export class Etape {
+
+    constructor(){
+
+    }
+    nom:string;
+    lieu:string;
+
   }
--- src/app/creation/creation.page.ts
+++ src/app/creation/creation.page.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AngularFirestore } from 'angularfire2/firestore';
+import { FirebaseService, Etape } from '../FirebaseService';
+
+import { ToastController } from '@ionic/angular';
+import { stringify } from 'querystring';
+
 @Component({
   selector: 'app-creation',
   templateUrl: './creation.page.html',
@@ -7,7 +13,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreationPage implements OnInit {
 
-  constructor() { }
+  public titreHistoire: string;
+  public nombreEtapes: number;
+  firebaseService: FirebaseService = new FirebaseService(this.afs);
+  public listeEtapes: Etape[] = [];
+  public etape1: boolean;
+  public etape2: boolean;
+  public etape3: boolean;
+  public etapeTest: Etape;
+
+  termine1(){
+    for(let i = 1;i<=this.nombreEtapes;i++){
+      this.etapeTest = new Etape();
+      this.etapeTest.lieu = "test";
+      this.etapeTest.nom = "testNom";
+      this.listeEtapes.push(this.etapeTest);
+    }
+    this.etape1=false;
+    this.etape2=true;
+  }
+
+  arrayOne(): any[] {
+    return Array(this.nombreEtapes);
+  }
+
+
+  createHistoire() {
+    this.firebaseService.nouvelleHistoire(this.titreHistoire, this.nombreEtapes, this.listeEtapes)
+      .then(async res => {
+        let toast = await this.toastController.create({
+          message: 'Vous avez bien créé votre histoire. Félicitations.',
+          duration: 3000
+        });
+        toast.present();
+        this.resetFields();
+      }, err => {
+        console.log(err)
+      })
+  }
+
+  resetFields() {
+    this.titreHistoire = "";
+    this.nombreEtapes = null;
+  }
+
+  constructor(
+    private afs: AngularFirestore,
+    public toastController: ToastController
+  ) {
+    this.etape1 = true;
+    this.etape2 = false;
+    this.etape3 = false;
+
+  }
 
   ngOnInit() {
   }
